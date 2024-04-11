ac9f58e08f41d82b296c6188f2bcfe63cdb3bf92
Carullini <165422387+Carullini@users.noreply.github.com>
2024-04-11 08:43:14 -0300

    Add files via upload

---
+++ meuamô.php
@@ -0,0 +1,18 @@
+<?php 
+
+// para o lovi
+// Hello lovi
+$nome = $_POST ['Caio']; 
+if ($nome=="Caio") {
+    for ($i = 1; $i <= 12; $i++) {
+        echo "<h1>Você é o meu amor!!!</h1>"; 
+    }
+}
+   else {
+    
+    echo"<h1 style='align: center'>Saia daqui seu licho!!!</h1>";
+}
+
+?> 
+
+
